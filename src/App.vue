f0ca5050fefac987af32de19b9d653e2799d45c5
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2021-01-06 21:59:14 +0000

    Spacex timeline example with initial setup for uka videos

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <h2> UKA Videos </h2>
+            </div>
+
+            <v-spacer></v-spacer>
+
+        </v-app-bar>
+
+        <v-main>
+            <v-container>
+                <v-timeline v-if="videos.length > 0">
+                    <VideoTimeLineItem
+                            v-for="video in videos"
+                            :key="video.date"
+                            :video="video"
+                    />
+                </v-timeline>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+    import axios from "axios";
+    // import LaunchTimelineItem from "./components/LaunchTimeLineItem.vue"
+    import VideoTimeLineItem from "./components/VideoTimeLineItem.vue"
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    export default {
+        name: "App",
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+        components: {
+            VideoTimeLineItem
+        },
+
+        data: () => ({
+            videos: [],
+        }),
+
+        created() {
+            axios.get('https://raw.githubusercontent.com/pavlakis/api-ukaikikai/master/videos.json')
+                .then(response => {
+
+                    for (const [key, object] of Object.entries(response.data)) {
+                        let video = object[0];
+                        video.date = `${key}`;
+                        // console.log(video);
+                        this.videos.push(video);
+                    }
+                });
+        },
+        mounted() {
+
+        }
+    };
+</script>
---
+++ src/components/VideoTimeLineItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-timeline-item>
+        <template v-slot:icon>
+            <v-avatar>
+                <span>{{ video.date }}</span>
+            </v-avatar>
+        </template>
+        <template v-slot:opposite>
+            <span>{{ video.title }}</span>
+        </template>
+        <v-card class="elevation-2">
+            <v-card-title class="headline">
+
+            </v-card-title>
+            <v-card-subtitle>
+                Title
+            </v-card-subtitle>
+            <v-card-text>
+                Description
+            </v-card-text>
+        </v-card>
+    </v-timeline-item>
+</template>
+
+<script>
+    // import moment from "moment";
+    export default {
+        props: ["video"],
+        methods: {
+          logs() {
+              console.log(this.video);
+              return 'stuff';
+          }
+        },
+        computed: {
+            launchDate() {
+                return 'n/a';
+            },
+        },
+    };
+</script>
